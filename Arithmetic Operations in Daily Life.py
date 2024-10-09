aa9712635b9cba0f47d33e49a4699568ca5efdc5
TiffaLanc <tukie91@icloud.com>
2024-10-09 19:41:05 -0400

    first commit

---
+++ Arithmetic Operations in Daily Life.py
@@ -0,0 +1,18 @@
+#Task 1 Grocery Store Math
+
+bread = 3.50
+juice = 3.50
+milk = 4.50
+
+print(bread + juice + milk)
+
+
+
+
+
+#Task 2 Bank Interest
+
+st_savings = 5000
+yrly_interest = 0.05
+
+print(st_savings * yrly_interest + st_savings)
---
+++ Showcase Your Dance Moves.py
@@ -0,0 +1,19 @@
+#Task 1 Code Correction
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+#Task 2 Your Mood Today
+
+print("How are you feeling today?")
+mood = input()    
+
+if mood == "happy":
+    print("That's great to hear!")
+else:
+    print("I hope your day gets better!")
